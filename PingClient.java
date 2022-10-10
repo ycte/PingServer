06a7b9f9182e86d9e3cf8937ab72825dce582eb6
ycte <114587040+ycte@users.noreply.github.com>
2022-10-10 11:23:29 +0800

    Commit initial source code and class

---
+++ PingClient.java
@@ -0,0 +1,168 @@
+
+/*
+ *
+ * PingClient sends 10 ping requests to the server
+ *
+ * Usage: java PingClient host port passwd
+ *
+ * Thanks: Instructor Qiao Xiang for the basic code of send and receive
+ *         CSDN for Time in the format of Java currentTimeMill, Timer
+ *
+ */
+
+// PingClient.java
+import java.io.IOException;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PingClient {
+    public static void main(String[] args) throws Exception {
+        // Get command line argument.
+        if (args.length <= 2) {
+            System.out.println("Usage: java PingClient host port passwd");
+            return;
+        }
+        String host = "user";
+        host = args[0];
+        InetAddress serverIPAddress = InetAddress.getByName(host);
+        int serverPort = 9876;
+        serverPort = Integer.parseInt(args[1]);
+        String passwd = "passwd";
+        passwd = args[2];
+        System.out.println("legal input:" + host + " " + serverPort + " " + passwd);
+
+        // Time in the format of Java currentTimeMill
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Start time:" + formatter.format(date));
+
+        // Create socket
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        // Initialize Timer and TimerTask
+        // Finalize for backup
+        final int[] timeOutNum = {0};
+        long[] delayArray = new long[10];
+        final boolean[] waiting = {true};
+        Timer timer = new Timer();
+        final int[] count = {0,0};
+        int finalServerPort = serverPort;
+        String finalPasswd = passwd;
+        final boolean[] sendFlag = {false, false, false, false, false, false, false, false, false, false,false};
+        // Set TimerTask
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                count[0]++;
+                // Time in the format of Java currentTimeMill
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(System.currentTimeMillis());
+                System.out.println(formatter.format(this.scheduledExecutionTime()) + "  Timer " + count[0] + " as below:");
+                sendFlag[count[0]] = true;
+                if (count[0] == 10) {
+                    System.out.println("!!!! Timer ends at " + count[0] + " !!!!");
+                    waiting[0] = false;
+                    this.cancel();
+                }
+            }
+        };
+        /*
+         * sendFlag[i]: become true at sec i, and return to false before send
+         *
+         * waiting[0]: be always true before sec 10
+         *
+         */
+        timer.schedule(task, 0, 1000);
+
+        // Loop for Ping
+        int countFlag = 0;
+        while (waiting[0]) {
+            System.out.print("");
+            if(sendFlag[count[0]]) {
+                sendFlag[count[0]] = false;
+                // Time in the format of Java currentTimeMill
+                SimpleDateFormat formatter1 = new SimpleDateFormat("HH:mm:ss");
+                Date date1 = new Date(System.currentTimeMillis());
+                String sentence = "PING " + (count[0] - 1) + " " + formatter1.format(date1) + " " + finalPasswd + " ";
+                // Format sendData
+                byte[] sendData = sentence.getBytes();
+                System.out.println("sendData: " + sentence);
+                System.out.println("sendData length = " + sendData.length);
+                System.out.println("destination: " + serverIPAddress + ":" + finalServerPort);
+                /*
+                 * timeSrcFlag: before send
+                 *
+                 * timeDstFlag: after receive
+                 */
+                long timeSrcFlag = 0;
+                try {
+                    // Construct and send datagram
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIPAddress, finalServerPort);
+                    timeSrcFlag = System.currentTimeMillis();
+                    clientSocket.send(sendPacket);
+                } catch (IOException e) {
+                    // throw new RuntimeException(e);
+                }
+                // Receive datagram and set timeout
+                byte[] receiveData = new byte[128];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                try {
+                    clientSocket.setSoTimeout(1000);
+                } catch (SocketException e) {
+                    // throw new RuntimeException(e);
+                }
+                try {
+                    clientSocket.receive(receivePacket);
+                } catch (SocketTimeoutException e) {
+                    // throw new RuntimeException(e);
+                    System.out.println("No reply is sent From Server!");
+                    // System.out.println("----------------------------------");
+                    // System.out.println();
+                    timeOutNum[0]++;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                long timeDstFlag = System.currentTimeMillis();
+                // Print delay
+                long receiveDelay = timeDstFlag - timeSrcFlag;
+                delayArray[countFlag] = receiveDelay;
+                System.out.println("delay: " + receiveDelay );
+                // Print data received
+                String sentenceFromServer = new String(receivePacket.getData());
+                System.out.println("From Server: " + sentenceFromServer);
+                System.out.println("----------------------------------");
+                System.out.println();
+                countFlag++;
+            }
+        }
+
+        // Close client socket
+        clientSocket.close();
+
+        // Count RTT and loss information
+        long maxRTT = 0, minRTT = 2048, sumRTT = 0;
+        double aveRTT;
+        for (int i = 0; i < 10; i++) {
+            // System.out.println(i + ":" + delayArray[i]);
+            if (delayArray[i] <= 1000) {
+                sumRTT = sumRTT + delayArray[i];
+            }
+            if (maxRTT < delayArray[i] && delayArray[i] <= 1000) {
+                maxRTT = delayArray[i];
+            }
+            if (minRTT > delayArray[i] ) {
+                minRTT = delayArray[i];
+            }
+        }
+        // Print RTT and loss information
+        aveRTT = sumRTT / 10.0;
+        System.out.println("RTTs Information:");
+        System.out.println("max RTT (except loss): " + maxRTT);
+        System.out.println("min RTT: " + minRTT);
+        System.out.println("average RTT: " + aveRTT);
+        System.out.println("loss rate: " + timeOutNum[0] * 1.0 / 10);
+    } // end of main
+}
---
+++ PingServer.java
@@ -0,0 +1,150 @@
+
+/*
+ *
+ * Server to process ping requests over UDP
+ *
+ * Usage: java PingServer port passwd [-delay delay] [-loss loss]
+ *
+ * Thanks: base on PingServer by instructor Qiao Xiang
+ */
+
+// PingServer.java
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class PingServer
+{
+    private static double LOSS_RATE = 0.3;
+    private static int AVERAGE_DELAY = 100; // milliseconds
+
+    public static void main(String[] args) throws Exception
+    {
+        // Get command line argument.
+        if (args.length <= 1) {
+            System.out.println("Usage: java PingServer port passwd [-delay delay] [-loss loss]");
+            return;
+        }
+        if (args.length >= 3) {
+            if(args[2].matches("-delay(.*)")){
+                AVERAGE_DELAY = Integer.parseInt(args[3]);
+            }
+            if(args[2].matches("-loss(.*)")){
+                LOSS_RATE = Double.parseDouble(args[3]);
+            }
+
+        }
+        if (args.length >= 5) {
+            if(args[4].matches("-delay(.*)")){
+                AVERAGE_DELAY = Integer.parseInt(args[5]);
+            }
+            if(args[4].matches("-loss(.*)")){
+                LOSS_RATE = Double.parseDouble(args[5]);
+            }
+
+        }
+
+        int port = Integer.parseInt(args[0]);
+        String passwd = args[1];
+        System.out.println("AVERAGE_DELAY: " + AVERAGE_DELAY + " LOSS_RATE: " + LOSS_RATE +
+                " port: " + port + " passwd: " + passwd);
+        // Create random number generator for use in simulating
+        // packet loss and network delay.
+        Random random = new Random();
+
+        // Create a datagram socket for receiving and sending
+        // UDP packets through the port specified on the
+        // command line.
+        DatagramSocket socket = new DatagramSocket(port);
+
+        // Processing loop.
+        while (true) {
+
+            // Create a datagram packet to hold incomming UDP packet.
+            DatagramPacket
+                    request = new DatagramPacket(new byte[128], 128);
+
+            // Block until receives a UDP packet.
+            // Wait
+            socket.receive(request);
+
+            // Print the received data, for debugging
+            printData(request);
+
+            // Check passwd and generate reply
+            String sentence = new String(request.getData());
+            sentence = sentence.replaceFirst("PING", "PINGECHO");
+            String[] clientItem = sentence.split(" ");
+            sentence = clientItem[0] + " " + clientItem[1] + " " + clientItem[2] + " " + clientItem[3];
+            if (passwd.compareTo(clientItem[3]) == 0) {
+                System.out.println("Passwd correct!");
+            }
+            else {
+                System.out.println("Wrong passwd!");
+            }
+
+            // Decide whether to reply, or simulate packet loss.
+            if (random.nextDouble() < LOSS_RATE) {
+                System.out.println(" Reply not sent.");
+                System.out.println("----------------------------------");
+                continue;
+            }
+
+            // Simulate prorogation delay.
+            Thread.sleep((int) (random.nextDouble() * 2 * AVERAGE_DELAY));
+
+            // Send reply.
+            InetAddress clientHost = request.getAddress();
+            int clientPort = request.getPort();
+            byte[] buf = sentence.getBytes();
+            DatagramPacket
+                    reply = new DatagramPacket(buf, sentence.length(),
+                    clientHost, clientPort);
+
+            socket.send(reply);
+
+            System.out.println(" Reply sent.");
+            System.out.println("----------------------------------");
+        } // end of while
+    } // end of main
+
+    /*
+     * Print ping data to the standard output stream.
+     */
+    private static void printData(DatagramPacket request)
+            throws Exception
+
+    {
+        // Obtain references to the packet's array of bytes.
+        byte[] buf = request.getData();
+
+        // Wrap the bytes in a byte array input stream,
+        // so that you can read the data as a stream of bytes.
+        ByteArrayInputStream bais
+                = new ByteArrayInputStream(buf);
+
+        // Wrap the byte array output stream in an input
+        // stream reader, so you can read the data as a
+        // stream of **characters**: reader/writer handles
+        // characters
+        InputStreamReader isr
+                = new InputStreamReader(bais);
+
+        // Wrap the input stream reader in a bufferred reader,
+        // so you can read the character data a line at a time.
+        // (A line is a sequence of chars terminated by any
+        // combination of \r and \n.)
+        BufferedReader br
+                = new BufferedReader(isr);
+
+        // The message data is contained in a single line,
+        // so read this line.
+        String line = br.readLine();
+
+        // Print host address and data received from it.
+        System.out.println("Received from " +
+                request.getAddress().getHostAddress() +
+                ": " +
+                new String(line) );
+    } // end of printData
+} // end of class
